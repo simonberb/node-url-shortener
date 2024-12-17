faac5a153578ff68d89227370417a47ccac1098d
Simon Berberich <157768932+simonberb@users.noreply.github.com>
2024-12-17 17:03:15 -0600

    Update encode.js

--- api/helpers/encode.js
+++ api/helpers/encode.js
@@ -1,1 +1,12 @@
+const alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+const base = alphabet.length;
 
+function encode(num) {
+  let encoded = '';
+  while (num) {
+    const remainder = num % base;
+    num = Math.floor(num / base);
+    encoded = alphabet[remainder].toString() + encoded;
+  }
+  return encoded;
+}
