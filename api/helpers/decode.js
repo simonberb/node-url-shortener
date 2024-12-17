9fe71645722b27ce5802c1c4b53a6c099c84f746
Simon Berberich <157768932+simonberb@users.noreply.github.com>
2024-12-17 17:03:40 -0600

    Update decode.js

--- api/helpers/decode.js
+++ api/helpers/decode.js
@@ -1,1 +1,12 @@
+function decode(str) {
+  let decoded = 0;
+  while (str) {
+    const index = alphabet.indexOf(str[0]);
+    const power = str.length - 1;
+    decoded += index * (base ** power);
+    str = str.substring(1);
+  }
+  return decoded;
+}
 
+module.exports.decode = decode;
